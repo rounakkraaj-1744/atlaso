136c75a1a08d6c411395369f75347dee02813c72
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2026-01-22 14:06:40 +0530

    more changes

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,6 +1,6 @@
 import type { InputHTMLAttributes } from 'react';
 import { forwardRef } from 'react';
-import { cn } from '../../lib/utils';
+import {cn} from "../../lib/utils"
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label?: string;
--- frontend/src/components/ui/Tabs.tsx
+++ frontend/src/components/ui/Tabs.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 import { useState } from 'react';
-import { cn } from '../../lib/utils';
+import {cn} from "../../lib/utils"
 
 interface Tab {
     id: string;
